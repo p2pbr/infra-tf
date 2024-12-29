f16043b3d74a256319c5a19a6952193ae32f9eab
Jaime Paiva <p2p.digitalbusiness@gmail.com>
2024-12-29 20:46:45 -0300

    configurado main

--- exercicio-1/main.tf
+++ exercicio-1/main.tf
@@ -1,22 +1,25 @@
 terraform {
   required_providers {
-    ssh = {
-      source  = "hashicorp/ssh"  # Corrigir o nome da fonte para "hashicorp/ssh"
-      version = ">= 2.0.0"       # Especificando uma versão mínima
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.0.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0.0"
     }
   }
 }
 
-provider "ssh" {
-  host        = var.host
-  user        = var.user
-  private_key = file(var.private_key_path)
+provider "tls" {}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
 resource "null_resource" "criar_diretorio" {
-  provisioner "remote-exec" {
-    inline = [
-      "mkdir -p ${var.diretorio}"
-    ]
+  provisioner "local-exec" {
+    command = "ssh -i ${tls_private_key.example.private_key_pem} ${var.user}@${var.host} 'mkdir -p ${var.diretorio}'"
   }
 }
